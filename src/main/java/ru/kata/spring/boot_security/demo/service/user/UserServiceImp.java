686a7207c340a4fdafbd6e670cadd4a5f2a480d7
Evgen12412 <evgeny.krd2016@yandex.ru>
2023-02-15 13:04:02 +0300

    4

--- src/main/java/ru/kata/spring/boot_security/demo/service/user/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/user/UserServiceImp.java
@@ -1,13 +1,11 @@
 package ru.kata.spring.boot_security.demo.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
 import java.util.List;
@@ -17,10 +15,13 @@ public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final RoleRepository roleRepository;
+
     @Autowired
-    public UserServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
 
@@ -33,7 +34,7 @@ public class UserServiceImp implements UserService {
     @Override
     @Transactional
     public void create(User user) {
-
+//        user.setRoleList();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
